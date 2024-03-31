2e6eb7ea4ad9acf4b55b82ecac6f051257808ac9
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-04-01 01:05:23 +0530

    Auth done with ClerkJS

---
+++ maths_sir_web/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,30 @@
+import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <main className='flex-center h-screen w-full bg-[url("/bg-signin.jpeg")] bg-cover bg-no-repeat  p-10 flex-col md:flex-row md:gap-10'>
+        <div className='flex-center w-full md:w-[50%]'>
+            <Image 
+                src={'/logo.svg'}
+                alt='Logo'
+                width={150}
+                height={0}
+                className='p-3 md:w-[300px]'
+            />
+            <div className='flex flex-col gap-1 w-[50%]'>
+            <p className='text-3xl md:text-5xl'>
+                Because of Maths
+            </p>
+            <p className='hidden md:block'>
+            Master math for boards & entrance exams with our comprehensive courses & expert guidance!
+            </p>
+            </div>
+        </div>
+            <SignIn />
+    </main>
+  )
+}
+
+export default SignInPage
