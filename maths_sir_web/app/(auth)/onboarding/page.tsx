8bf57426caaafe9bbeb3e0236fd047686f38ebb8
prabuddha369 <prabuddhapiku@gmail.com>
2024-05-02 22:48:11 +0530

    route_change

--- maths_sir_web/app/(auth)/onboarding/page.tsx
+++ maths_sir_web/app/(auth)/onboarding/page.tsx
@@ -1,22 +1,23 @@
 "use client";
 import { addUsersToDB } from "@/utils";
 import { useRouter } from "next/navigation";
+import Navbar from "@/components/shared/Navbar";
 import Form from "@/components/shared/Form";
 const Onboarding = () => {
   const router = useRouter();
-   const handleAddUser = ({name, email, number, school, std, message}:{
-    name:string,
-    email:string,
-    number?:string,
-    school?:string,
-    std?:string,
-    message?:string
+  const handleAddUser = ({ name, email, number, school, std, message }: {
+    name: string,
+    email: string,
+    number?: string,
+    school?: string,
+    std?: string,
+    message?: string
   }) => {
     addUsersToDB({
       name,
       email: email as string,
       phoneNumber: number!,
-      school:school!,
+      school: school!,
       courses: [],
       class: std!,
     });
@@ -24,7 +25,12 @@ const Onboarding = () => {
     router.push("/");
   };
 
-  return <Form type="onboarding" onSubmit={handleAddUser} />;
+  return (
+    <section>
+      <Navbar show={false} />
+      <Form type="onboarding" onSubmit={handleAddUser} />
+    </section>
+  );
 };
 
 export default Onboarding;
--- maths_sir_web/components/shared/Navbar.tsx
+++ maths_sir_web/components/shared/Navbar.tsx
@@ -8,13 +8,12 @@ interface NavbarProps {
   show: boolean;
 }
 const Navbar: React.FC<NavbarProps> = ({ show }) => {
-  const {user} = useUser();
+  const { user } = useUser();
 
   return (
     <nav
-      className={`text-xl w-full ps-5 pe-10 flex justify-between bg-gradient-to-b from-[#9747FF]/50 to-[#FFFFFF]/10 backdrop-blur-md top-0 left-0 sticky z-10 transition-transform duration-300 transform ${
-        show ? "bg-gradient-to-b from-[#9747FF]/50 to-[#FFFFFF]/10" : "bg-gradient-to-b from-[#9E53FF]/60 to-[#9E53FF]/40 backdrop-blur-lg shadow-sm shadow-[#9E53FF]/10"
-      }`}
+      className={`text-xl w-full ps-5 flex justify-between backdrop-blur-md top-0 left-0 sticky z-10 transition-transform duration-300 transform ${show ? "bg-gradient-to-b from-[#9747FF]/50 to-[#FFFFFF]/10" : "bg-gradient-to-b from-[#9E53FF]/60 to-[#9E53FF]/40 backdrop-blur-lg shadow-sm shadow-[#9E53FF]/10"
+        }`}
     >
       <Link href={"/"} className="flex flex-row place-items-center justify-center">
         <Image
@@ -24,21 +23,24 @@ const Navbar: React.FC<NavbarProps> = ({ show }) => {
           alt="Logo"
           className="md:w-[60px]"
         />
-        <span className={`ms-2 text-black leading-[1.25rem] text-xl ${show?"opacity-0 hidden":"opacity-100 block transition-opacity ease-in-out duration-1000"}`}>Because<br/>of Maths</span>
+        <span className={`ms-2 text-black leading-[1.25rem] text-xl ${show ? "opacity-0 hidden" : "opacity-100 block transition-opacity ease-in-out duration-1000"}`}>Because<br />of Maths</span>
       </Link>
       <div className="flex-center justify-between gap-2 md:gap-10 text-[13px] md:text-lg ">
-        {navLinks.map((link) => (
-          <Link href={link.href} key={link.label}>
-            {link.label}
-          </Link>
-        ))}
+        <div className="flex flex-row gap-2 md:gap-8">
+          {navLinks.map((link) => (
+            <Link href={link.href} key={link.label}>
+              {link.label}
+            </Link>
+          ))}
+        </div>
         <SignedIn>
-          {/* Mount the UserButton component */}
-          <UserButton afterSignOutUrl="/" showName />
+          <div className="bg-white p-2 rounded-l-xl shadow-sm">
+            <UserButton showName afterSignOutUrl="/"/>
+          </div>
         </SignedIn>
         <SignedOut>
-          <div className="flex items-center justify-center rounded-2xl shadow md:p-1 px-2 md:px-5 bg-[#FDD7BB] text-[11px] md:text-lg">
-          <SignInButton  />
+          <div className="flex items-center pe-3 md:pe-10 justify-center rounded-2xl shadow md:p-1 px-2 md:px-5 bg-[#FDD7BB] text-[16px] md:text-lg">
+            <SignInButton />
           </div>
         </SignedOut>
       </div>
